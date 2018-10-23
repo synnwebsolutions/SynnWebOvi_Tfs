5cfef4e724692181837e80104b38b5acf0b066e9
adelasm <adelasm@USER2-60511>
2018-10-23 15:03:30 +0300

    Go Prod .

--- WebSimplify/WebSimplify/js/synnJavaScripts.js
+++ WebSimplify/WebSimplify/js/synnJavaScripts.js
@@ -3,19 +3,19 @@
     ArrangeTableFilters(); // finds grid inner tbody and sets the class
     ArrangeASPcalendar();
 
-    $(".tablefilter").on("keyup", function () {
-        var value = $(this).val().toLowerCase();
-            $(".datatofilter tr").filter(function () {
-                {
-                    var rowText = $(this).text().toLowerCase();
-                    $(this).toggle(rowText.lenght > 0 && rowText.indexOf(value) > -1);
-                }
-            });
-    });
-
 });
 function ArrangeTableFilters() {
     $(".tabletofilter").find("tbody").addClass('datatofilter');
+
+    $(".tablefilter").on("keyup", function () {
+        var value = $(this).val().toLowerCase();
+        $(".datatofilter tr").filter(function () {
+            {
+                var rowText = $(this).text().toLowerCase();
+                $(this).toggle(rowText.length > 0 && rowText.indexOf(value) > -1);
+            }
+        });
+    });
 }
 
 function ArrangeASPcalendar()
